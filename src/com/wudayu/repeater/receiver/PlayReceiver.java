f01b83023d1dde0242abd89cc04f540a03f5b7fa
David Wu <wudayu@gmail.com>
2014-11-22 21:01:30 +0800

    add some receivers

---
+++ src/com/wudayu/repeater/receiver/PlayReceiver.java
@@ -0,0 +1,24 @@
+package com.wudayu.repeater.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ *
+ * @author: Wu Dayu
+ * @En_Name: David Wu
+ * @E-mail: wudayu@gmail.com
+ * @Created Time: Nov 21, 2014, 5:55:39 PM
+ * @Description: David Wu created this file.
+ *
+ **/
+
+public class PlayReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		
+	}
+
+}
